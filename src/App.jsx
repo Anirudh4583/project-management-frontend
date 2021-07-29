25e501abaf7c80bc59a5b05a33feec4859e59ffb
anirudh4583 <201951024@iiitvadodara.ac.in>
2021-07-29 22:42:12 +0530

    react app with csa

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
