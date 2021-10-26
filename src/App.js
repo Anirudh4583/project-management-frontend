fd820d11376b2df6a8bd10534ee12a9ee5af1e32
raghusodani <201951118@iiitvadodara.ac.in>
2021-10-26 18:11:39 +0530

    Announcement card basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import AnnouncementCard from './components/AnnouncementCard/AnnouncementCard'
 import { Home, Login, Dashboard } from './views/viewIndex'
 import PublicRoute from './utils/Routers/PublicRoute'
 import PrivateRoute from './utils/Routers/PrivateRoute'
@@ -13,9 +13,11 @@ function App() {
       <Router>
         <div className="content">
           <Switch>
+
             <PublicRoute exact path="/" component={Home} />
             <PublicRoute path="/login" component={Login} />
             <PrivateRoute path="/:role/dashboard" component={Dashboard} />
+            <PrivateRoute path="/announcement" component={AnnouncementCard} />
             <PrivateRoute path="/form/:formId" component={FillForm} />
             <PrivateRoute
               path="/create-announcement"
